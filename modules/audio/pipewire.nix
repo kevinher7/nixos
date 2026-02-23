8c509eddd4044a3713414ae18a63ab45f1e770bb
Kevin Hernandez <kevinhernem@gmail.com>
2026-02-24 01:33:01 +0900

    fix: add missing environment setting

--- modules/audio/pipewire.nix
+++ modules/audio/pipewire.nix
@@ -5,6 +5,10 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  # Add this for standalone WM setups
+  # Source: https://github.com/NixOS/nixpkgs/issues/390071
+  environment.pathsToLink = [ "/share/wireplumber" ];
 }
 
 
