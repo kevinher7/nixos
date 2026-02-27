7b95c0f8b327e7c5906d7225112c94a243330b60
Kevin Hernandez <kevinhernem@gmail.com>
2026-02-27 20:21:14 +0900

    feat: add yank around functions and classes

--- home/nixvim/plugins/treesitter.nix
+++ home/nixvim/plugins/treesitter.nix
@@ -20,6 +20,18 @@
         settings = {
           highlight.enable = true;
           indent.enable = true;
+
+          textobjects = {
+            select = {
+              enable = true;
+              keymaps = {
+                "af" = "@function.outer";
+                "if" = "@function.inner";
+                "ac" = "@class.outer";
+                "ic" = "@class.inner";
+              };
+            };
+          };
         };
       };
     };
