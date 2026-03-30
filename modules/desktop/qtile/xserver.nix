00e839106e3758b24b2c1b956a5e95f986410706
Kevin Hernandez <kevinhernem@gmail.com>
2026-03-30 22:29:26 +0900

    fix: let stylix handle wallpaper

--- modules/desktop/qtile/xserver.nix
+++ modules/desktop/qtile/xserver.nix
@@ -5,7 +5,6 @@ _:
     desktopManager.runXdgAutostartIfNone = true;
     xkb.layout = "jp";
     displayManager.sessionCommands = ''
-      xwallpaper --zoom ${../../assets/wallpapers/girl-reading-book.png}
       xset r rate 400 35 &
     '';
   };
