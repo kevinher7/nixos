167b3bad5e8e15e39ee8f042f38e6f4ad79fd0c0
Kevin Hernandez <kevinhernem@gmail.com>
2026-03-30 22:11:12 +0900

    fix: invalid path

--- home/desktops/qtile/default.nix
+++ home/desktops/qtile/default.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   xdg.configFile."qtile" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/kevin/nixos-config/home/desktop/qtile";
+    source = config.lib.file.mkOutOfStoreSymlink "/home/kevin/nixos-config/home/desktops/qtile";
     recursive = true;
   };
 }
