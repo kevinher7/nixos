26420bef209edd3e552fb34dab2fdef99b0b3021
Kevin Hernandez <kevinhernem@gmail.com>
2026-04-11 17:09:52 +0900

    feat: add power module + import packages module on server

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,55 +1,33 @@
-{ pkgs, hostname, ... }:
+{ hostname, profile, ... }:
 {
   imports = [
     ./hardware-configuration.nix
     ../../modules/system.nix
+    ../../modules/packages.nix
     ../../modules/docker.nix
     ../../modules/desktop/qtile
     ../../modules/networking
     ../../modules/login
+    ../../modules/power
   ];
-
-  myModules.networking = {
-    enable = true;
-    hostname = hostname;
-    tailscale = {
-      enable = true;
-      openFirewall = true;
-      ssh = true;
-    };
-  };
 
   time.timeZone = "Asia/Tokyo";
 
-  services.logind.lidSwitch = "ignore";
-  services.logind.lidSwitchExternalPower = "ignore";
+  myModules = {
+    networking = {
+      enable = true;
+      hostname = hostname;
+      tailscale = {
+        enable = true;
+        openFirewall = true;
+        ssh = true;
+      };
+    };
 
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
-
-  users.users.uribo = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "video" ]; # Enable 'sudo' for the user.
-    packages = with pkgs; [
-      tree
-    ];
+    power = {
+      enable = true;
+      profile = profile;
+    };
   };
-
-  programs.firefox.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    git
-    alacritty
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
 }
 
