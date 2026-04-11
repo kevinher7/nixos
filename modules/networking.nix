d85bb0eb6ad2c028330505be835722f28748674b
Kevin Hernandez <kevinhernem@gmail.com>
2026-04-11 15:57:38 +0900

    feat: modularize networking

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,25 +1,61 @@
-_:
+{ config, lib, ... }:
+let
+  cfg = config.myModules;
+in
 {
-  networking.networkmanager.enable = true;
+  options.myModules.networking = {
+    enable = lib.mkEnableOption "custom networking configuration";
 
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 9300 ];
-    allowedUDPPorts = [ 5353 ];
-  };
+    hostName = lib.mkOption {
+      type = lib.types.str;
+      default = "nixos";
+      description = "The hostname of the machine";
+    };
 
-  services = {
-    tailscale.enable = true;
-    openssh.enable = true;
+    tailscale = {
+      enable = lib.mkEnableOption "Tailscale VPN";
 
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      publish = {
-        enable = true;
-        userServices = true;
+      openFirewall = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Open firewall for Tailscale (UDP 41641)";
+      };
+
+      ssh = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable Tailscale SSH server (--ssh flag)";
       };
     };
   };
 
+  config = lib.mkIf cfg.networking.enable {
+    networking.hostName = cfg.networking.hostName;
+    networking.networkmanager.enable = true;
+
+    networking.firewall = {
+      enable = true;
+      allowedTCPPorts = [ 9300 ];
+      allowedUDPPorts = [ 5353 ];
+    };
+
+    services = {
+      tailscale = lib.mkIf cfg.networking.tailscale.enable {
+        enable = true;
+        openFirewall = cfg.networking.tailscale.openFirewall;
+        extraSetFlags = lib.optional cfg.networking.tailscale.ssh "--ssh";
+      };
+
+      openssh.enable = true;
+
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+        publish = {
+          enable = true;
+          userServices = true;
+        };
+      };
+    };
+  };
 }
