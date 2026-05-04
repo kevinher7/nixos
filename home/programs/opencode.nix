985cf51df5bbce83b1ca0f01c4ab986f62e0924f
Kevin Hernandez <kevinhernem@gmail.com>
2026-05-05 02:24:28 +0900

    feat: add web server to opencode

--- home/programs/opencode.nix
+++ home/programs/opencode.nix
@@ -3,5 +3,11 @@ _:
   programs.opencode = {
     enable = true;
     # tui.themes = "nord";
+
+    web = {
+      enable = true;
+      extraArgs = [ "--hostname" "0.0.0.0" "--port" "4096" ];
+      # TODO: add environmentFile with sops-nix password
+    };
   };
 }
