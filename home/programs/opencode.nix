938f1e0e857c2a50a169211375266bacaa3480d2
Kevin Hernandez <kevinhernem@gmail.com>
2026-05-05 02:40:52 +0900

    chore: replace web option with systemd service

--- home/programs/opencode.nix
+++ home/programs/opencode.nix
@@ -1,13 +1,25 @@
-_:
+{ config, lib, ... }:
 {
   programs.opencode = {
     enable = true;
     # tui.themes = "nord";
+  };
 
-    web = {
-      enable = true;
-      extraArgs = [ "--hostname" "0.0.0.0" "--port" "4096" ];
-      # TODO: add environmentFile with sops-nix password
+  systemd.user.services.opencode-web = {
+    Unit = {
+      Description = "OpenCode Web Service";
+      After = [ "network.target" ];
+    };
+
+    Service = {
+      ExecStart = "${lib.getExe config.programs.opencode.package} serve --hostname 0.0.0.0 --port 4096";
+      Restart = "always";
+      RestartSec = 5;
+      # TODO: add EnvironmentFile with sops-nix password
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
     };
   };
 }
