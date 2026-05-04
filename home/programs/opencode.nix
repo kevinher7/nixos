58282dbc1e5b0c603e228b915386344d66d330a9
Kevin Hernandez <153143551+kevinher7@users.noreply.github.com>
2026-05-05 03:07:33 +0900

    feat: add opencode and web server (#43)
    * feat: add opencode to home manager
    
    * feat: manage theme via opencode
    
    * chore: updte flakes
    
    * chore: add alias for opencode
    
    * feat: add web server to opencode
    
    * chore: replace web option with systemd service
    
    * fix: set working directory for opencode service to nixos-config

---
+++ home/programs/opencode.nix
@@ -0,0 +1,26 @@
+{ config, lib, ... }:
+{
+  programs.opencode = {
+    enable = true;
+    # tui.themes = "nord";
+  };
+
+  systemd.user.services.opencode-web = {
+    Unit = {
+      Description = "OpenCode Web Service";
+      After = [ "network.target" ];
+    };
+
+    Service = {
+      ExecStart = "${lib.getExe config.programs.opencode.package} serve --hostname 0.0.0.0 --port 4096";
+      WorkingDirectory = "/home/uribo/nixos-config";
+      Restart = "always";
+      RestartSec = 5;
+      # TODO: add EnvironmentFile with sops-nix password
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+}
