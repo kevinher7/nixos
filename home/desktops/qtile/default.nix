2e6539725c1086b65a7a13dcc0ab1c21618b4916
Kevin Hernandez <kevinhernem@gmail.com>
2026-05-06 03:23:41 +0900

    style: format

--- home/desktops/qtile/default.nix
+++ home/desktops/qtile/default.nix
@@ -1,7 +1,4 @@
-{
-  config,
-  ...
-}: {
+{config, ...}: {
   xdg.configFile."qtile" = {
     source = config.lib.file.mkOutOfStoreSymlink "/home/kevin/nixos-config/home/desktops/qtile";
     recursive = true;
