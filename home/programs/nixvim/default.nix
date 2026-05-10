76fe931c8d1b5d08ff0d49ed8a2b5d439bf0bf73
Kevin Hernandez <kevinhernem@gmail.com>
2026-05-10 15:41:03 +0900

    style: format

--- home/programs/nixvim/default.nix
+++ home/programs/nixvim/default.nix
@@ -12,126 +12,126 @@ in {
   ];
 
   config = lib.mkIf cfg.enable {
-  home.packages = with pkgs; [
-    # Nixvim
-    ripgrep
-    alejandra
-    ruff
-    clang-tools
-    typstyle
+    home.packages = with pkgs; [
+      # Nixvim
+      ripgrep
+      alejandra
+      ruff
+      clang-tools
+      typstyle
 
-    # Linters
-    shellcheck
-    markdownlint-cli
-    statix
-  ];
-
-  programs.nixvim = {
-    enable = true;
-
-    defaultEditor = true;
-    luaLoader.enable = true;
-
-    colorschemes.catppuccin = {
-      enable = true;
-
-      settings.background = {
-        dark = "frappe";
-        light = "latte";
-      };
-    };
-
-    clipboard = {
-      providers = {
-        xsel.enable = true;
-      };
-
-      # register = "unnamedplus";
-    };
-
-    opts = {
-      relativenumber = true;
-      number = true;
-
-      breakindent = true;
-
-      ignorecase = true;
-      smartcase = true;
-
-      scrolloff = 8;
-
-      cursorline = true;
-      spell = false;
-
-      # Tab Options
-      tabstop = 4;
-      shiftwidth = 4;
-      expandtab = true;
-      autoindent = true;
-    };
-
-    autoGroups = {
-      kickstart-highlight-yank = {
-        clear = true;
-      };
-    };
-
-    autoCmd = [
-      {
-        event = ["TextYankPost"];
-        desc = "Highliht when yanking text";
-        group = "kickstart-highlight-yank";
-        callback = {
-          __raw = ''
-
-            function()
-              vim.hl.on_yank()
-            end
-          '';
-        };
-      }
+      # Linters
+      shellcheck
+      markdownlint-cli
+      statix
     ];
 
-    diagnostic = {
-      settings = {
-        severity_sort = true;
-        float = {
-          boarder = "rounder";
-          source = "if_many";
+    programs.nixvim = {
+      enable = true;
+
+      defaultEditor = true;
+      luaLoader.enable = true;
+
+      colorschemes.catppuccin = {
+        enable = true;
+
+        settings.background = {
+          dark = "frappe";
+          light = "latte";
+        };
+      };
+
+      clipboard = {
+        providers = {
+          xsel.enable = true;
         };
 
-        underline = {
-          severity.__raw = ''vim.diagnostic.severity.ERROR'';
+        # register = "unnamedplus";
+      };
+
+      opts = {
+        relativenumber = true;
+        number = true;
+
+        breakindent = true;
+
+        ignorecase = true;
+        smartcase = true;
+
+        scrolloff = 8;
+
+        cursorline = true;
+        spell = false;
+
+        # Tab Options
+        tabstop = 4;
+        shiftwidth = 4;
+        expandtab = true;
+        autoindent = true;
+      };
+
+      autoGroups = {
+        kickstart-highlight-yank = {
+          clear = true;
         };
+      };
 
-        signs.__raw = ''
-           vim.g.have_nerd_font and {
-            text = {
-              [vim.diagnostic.severity.ERROR] = '󰅚 ',
-              [vim.diagnostic.severity.WARN] = '󰀪 ',
-              [vim.diagnostic.severity.INFO] = '󰋽 ',
-              [vim.diagnostic.severity.HINT] = '󰌶 ',
-            },
-          } or {}
-        '';
+      autoCmd = [
+        {
+          event = ["TextYankPost"];
+          desc = "Highliht when yanking text";
+          group = "kickstart-highlight-yank";
+          callback = {
+            __raw = ''
 
-        virtual_text = {
-          source = "if_many";
-          spacing = 2;
-          format.__raw = ''
-            function(diagnostic)
-              local diagnostic_message = {
-                [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                [vim.diagnostic.severity.WARN] = diagnostic.message,
-                [vim.diagnostic.severity.INFO] = diagnostic.message,
-                [vim.diagnostic.severity.HINT] = diagnostic.message,
-              }
-              return diagnostic_message[diagnostic.severity]
-            end
+              function()
+                vim.hl.on_yank()
+              end
+            '';
+          };
+        }
+      ];
+
+      diagnostic = {
+        settings = {
+          severity_sort = true;
+          float = {
+            boarder = "rounder";
+            source = "if_many";
+          };
+
+          underline = {
+            severity.__raw = ''vim.diagnostic.severity.ERROR'';
+          };
+
+          signs.__raw = ''
+             vim.g.have_nerd_font and {
+              text = {
+                [vim.diagnostic.severity.ERROR] = '󰅚 ',
+                [vim.diagnostic.severity.WARN] = '󰀪 ',
+                [vim.diagnostic.severity.INFO] = '󰋽 ',
+                [vim.diagnostic.severity.HINT] = '󰌶 ',
+              },
+            } or {}
           '';
+
+          virtual_text = {
+            source = "if_many";
+            spacing = 2;
+            format.__raw = ''
+              function(diagnostic)
+                local diagnostic_message = {
+                  [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                  [vim.diagnostic.severity.WARN] = diagnostic.message,
+                  [vim.diagnostic.severity.INFO] = diagnostic.message,
+                  [vim.diagnostic.severity.HINT] = diagnostic.message,
+                }
+                return diagnostic_message[diagnostic.severity]
+              end
+            '';
+          };
         };
       };
     };
-  };
   };
 }
