b50d4a21d63b7692c6ed7e7e0ff7d1ee17d9c56c
Kevin Hernandez <kevinhernem@gmail.com>
2026-05-31 14:39:55 +0900

    fix: add linger to server user

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -1,11 +1,16 @@
 {
   pkgs,
   username,
+  profile,
+  lib,
   ...
 }: {
   users.users.${username} = {
     isNormalUser = true;
     extraGroups = ["wheel" "video"];
     shell = pkgs.bash;
+
+    # Enable lingering on the server so user services keep running after the login session ends.
+    linger = lib.mkIf (profile == "server") true;
   };
 }
