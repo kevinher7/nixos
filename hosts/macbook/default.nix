1a49aa56206215f424deab295b2734cfb190d799
Kevin Hernandez <153143551+kevinher7@users.noreply.github.com>
2026-06-12 03:24:25 +0900

    refactor: add darwin module with homebrew (#115)

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -7,6 +7,7 @@
   imports = [
     ../../modules/core/packages.nix
     ../../modules/vars
+    ../../modules/darwin
   ];
 
   myVars = {
@@ -62,50 +63,5 @@
   users.users.${username} = {
     home = "/Users/${username}";
     shell = pkgs.zsh;
-  };
-
-  homebrew = {
-    enable = true;
-
-    taps = [
-      "nikitabobko/tap"
-    ];
-
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "uninstall";
-      upgrade = true;
-      # Homebrew 5.1.14+ requires a force flag for `--cleanup`; run it non-interactively.
-      extraFlags = ["--force-cleanup"];
-    };
-
-    greedyCasks = true;
-
-    brews = [
-      "ansible"
-      "sshpass"
-      "tfenv"
-    ];
-
-    casks = [
-      "1password"
-      "1password-cli"
-      "aerospace"
-      "cap"
-      "codex"
-      "dbeaver-community"
-      "ghostty"
-      "docker-desktop"
-      "google-chrome"
-      "openvpn-connect"
-      "postman"
-      "raycast"
-      "stats"
-      "steam"
-      "tailscale-app"
-      "visual-studio-code"
-      "wispr-flow"
-      "zoom"
-    ];
   };
 }
