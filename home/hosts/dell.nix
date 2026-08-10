4bc4287c15c116fd01750cc21c201c644f46ade1
Kevin Hernandez <153143551+kevinher7@users.noreply.github.com>
2026-08-11 00:39:26 +0900

    fix(bluetooth): route Echo audio to Bluetooth output (#174)
    * fix(bluetooth): prevent Dell from acting as A2DP sink
    
    * refactor(bluetooth): consolidate audio role config
    
    * fix(dell): refine bluetooth audio settings

--- home/hosts/dell.nix
+++ home/hosts/dell.nix
@@ -9,7 +9,14 @@
     rquickshare.enable = true;
     nixvim.enable = true;
     opencode.enable = true;
+    zen-browser.enable = true;
   };
+
+  xdg.configFile."wireplumber/wireplumber.conf.d/99-bluez-roles.conf".text = ''
+    override.monitor.bluez.properties = {
+      bluez5.roles = [ a2dp_source bap_source hfp_ag ]
+    }
+  '';
 
   home.packages = with pkgs; [
     playerctl
