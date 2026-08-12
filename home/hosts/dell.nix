111eb9d22081bb7459676c242aedd7da2091d675
Kevin Hernandez <kevinhernem@gmail.com>
2026-08-13 00:44:39 +0900

    fix(dell): place external monitor left of laptop

--- home/hosts/dell.nix
+++ home/hosts/dell.nix
@@ -11,6 +11,34 @@
     zen-browser.enable = true;
   };
 
+  services.kanshi.settings = [
+    {
+      profile.name = "docked";
+      profile.outputs = [
+        {
+          criteria = "LG Electronics 32inch LG FHD *";
+          status = "enable";
+          position = "0,0";
+        }
+        {
+          criteria = "eDP-1";
+          status = "enable";
+          position = "1920,0";
+        }
+      ];
+    }
+    {
+      profile.name = "undocked";
+      profile.outputs = [
+        {
+          criteria = "eDP-1";
+          status = "enable";
+          position = "0,0";
+        }
+      ];
+    }
+  ];
+
   xdg.configFile."wireplumber/wireplumber.conf.d/99-bluez-roles.conf".text = ''
     override.monitor.bluez.properties = {
       bluez5.roles = [ a2dp_source bap_source hfp_ag ]
