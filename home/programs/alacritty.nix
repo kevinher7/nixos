027fa7cb7c2ac3f8b0d8135dfef92581d63aa50a
Kevin Hernandez <kevinhernem@gmail.com>
2026-09-20 14:50:31 +0900

    fix(alacritty): add native terminal shortcuts

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -25,6 +25,24 @@ in {
         font = {
           size = lib.mkForce 10;
         };
+
+        keyboard.bindings = [
+          {
+            key = "C";
+            mods = "Control";
+            action = "Copy";
+          }
+          {
+            key = "V";
+            mods = "Control";
+            action = "Paste";
+          }
+          {
+            key = "N";
+            mods = "Control|Shift";
+            action = "CreateNewWindow";
+          }
+        ];
       };
     };
   };
